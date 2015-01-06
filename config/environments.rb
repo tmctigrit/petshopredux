233c2ac2e8eaf5536935967ebfc1cebdc534fed9
tmctigrit <mctigrit.tricia@gmail.com>
2015-01-06 16:33:15 -0600

    Change require_relative path to petshop.rb

--- config/environments.rb
+++ config/environments.rb
@@ -1,6 +1,6 @@
 require 'active_record'
 require 'active_record_tasks'
-require_relative 'lib/petshop.rb' #the path to our application file
+require_relative '../lib/petshop.rb' #the path to our application file
 
 ActiveRecord::Base.establish_connection(
   :adapter => 'postgresql',
