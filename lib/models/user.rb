32d9ab923b85bd524594da641b6fc32f4d989b4f
tmctigrit <mctigrit.tricia@gmail.com>
2015-01-06 23:23:57 -0600

    Add require active_record to user.rb file

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,10 +1,12 @@
+require 'active_record'
+
 class User < ActiveRecord::Base
   validates :username, :presence => {:message => "Username can't be blank."}
   validates :username, :uniqueness => {:message => "Username already exists."}
   validates :username, :length => { in: 4..12 }
   validates :password, :presence => {:message => "Password can't be blank."}
   validates :password, :length => { :minimum => 6, :message => "Must be at least 6 characters."}
-  
+
   ## WIP
   #validates :password, :format => { with: /\A[a-zA-Z]+\z/, :message => "Must have one capital and one number."}
 end
