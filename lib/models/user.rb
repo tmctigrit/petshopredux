6651a3e235c3bdf6587ee011a8f36104abacc877
tmctigrit <mctigrit.tricia@gmail.com>
2015-01-07 09:43:30 -0600

    Small change

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -3,9 +3,10 @@ require 'active_record'
 class User < ActiveRecord::Base
   validates :username, :presence => {:message => "Username can't be blank."}
   validates :username, :uniqueness => {:message => "Username already exists."}
-  validates :username, :length => { in: 4..12 }
+  validates :username, :length => 4..12
   validates :password, :presence => {:message => "Password can't be blank."}
   validates :password, :length => { :minimum => 6, :message => "Must be at least 6 characters."}
+  # validates :password, format: {with: /^[^-]\w+$/ }
 
   ## WIP
   #validates :password, :format => { with: /\A[a-zA-Z]+\z/, :message => "Must have one capital and one number."}
